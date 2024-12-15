1ea4411bc9da34723c30594f9aef075b32575052
lasse-numerous <lasse.thomsen@numerous.com>
2024-12-15 11:11:39 +0100

    fix(apps): First working version.
    Update project configuration and documentation
    
    - Added 'build' to .gitignore to exclude build artifacts.
    - Removed the unused 'apps.md' documentation file.
    - Updated 'mkdocs.yml' to remove the API Reference section.
    - Added 'numpy' to the dependencies in 'pyproject.toml'.
    - Enhanced the 'README.md' with a clearer description of the framework and its features, including a new section on getting started and app structure.
    - Changed the Uvicorn host in 'app.py' to '127.0.0.1' for better local development compatibility.
    
    These changes improve project organization, documentation clarity, and dependency management.

--- src/numerous/apps/bootstrap_app/app.py
+++ src/numerous/apps/bootstrap_app/app.py
@@ -43,4 +43,4 @@ app = create_app(template="index.html.j2", dev=True, app_generator=run_app)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
